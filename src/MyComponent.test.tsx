65161b40e0800780bd069005e142f8cef98926f7
Kazuya Tanimoto <tanimoto@byte-lark.com>
2024-02-21 09:30:04 +0900

    exclude e2e test from vitest

--- src/MyComponent.test.tsx
+++ src/MyComponent.test.tsx
@@ -1,9 +1,12 @@
-import "@testing-library/jest-dom";
+// @vitest-environment jsdom
+import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
 import { MyComponent } from "./MyComponent.tsx";
 
 test("「Hello Test」が描画されている", () => {
   render(<MyComponent />);
   // screen.debug();
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-call */
   expect(screen.getByText("Hello Test")).toBeInTheDocument();
 });

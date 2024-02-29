838f6ae66deddccb9cff270be304db53d0149096
Kazuya Tanimoto <tanimoto@byte-lark.com>
2024-02-29 09:57:17 +0900

    Revert "Adjusting vitest setting"
    This reverts commit e591ac1a66c1b1828d3d8bf856ca911a4e375b8e.

--- src/MyComponent.test.tsx
+++ src/MyComponent.test.tsx
@@ -1,13 +1,12 @@
+// @vitest-environment jsdom
+import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
 import { MyComponent } from "./MyComponent.tsx";
 
 test("「Hello Test」が描画されている", () => {
   render(<MyComponent />);
   // screen.debug();
-  const element = screen.getByText("Hello Test");
-  // const element2 = screen.queryByText("Hello Test");
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  expect(element).toBeInTheDocument();
-  // expect(screen.getByText("Hello Test")).toBeInTheDocument();
-  // expect(screen.queryByText("Hello Test")).toBeInTheDocument();
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-call */
+  expect(screen.getByText("Hello Test")).toBeInTheDocument();
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
-    include: ["src/**/*.{test,spec}.ts?(x)"],
-    globals: true,
-    setupFiles: "./vitest.setup.ts"
+    include: ['src/**/*.{test,spec}.ts?(x)'],
   }
-});
+})

f80d5b8fe9f7cb34744eef8b87bd2d390cfe3f69
Kazuya Tanimoto <tanimoto@byte-lark.com>
2024-03-22 09:46:11 +0900

    Small improvement

--- src/features/about/components/molecules/CareerItem.tsx
+++ src/features/about/components/molecules/CareerItem.tsx
@@ -1,28 +1,32 @@
 import type { JSX } from "react";
-import { memo } from "react";
 import { Flex } from "@chakra-ui/react";
 import type { CareerItem as Props } from "../../types/Career.ts";
 import { DateRange } from "../atoms/DateRange.tsx";
 import { ProjectSummary } from "../atoms/ProjectSummary.tsx";
 
-export const CareerItem = memo(
-  ({ title, summary, keywords, color, from, to }: Props): JSX.Element => {
-    return (
-      <Flex
-        px={4}
-        py={2}
-        w={500}
-        borderWidth={1}
-        borderColor={color}
-        rounded="full"
-        gap={2}
-        h={125}
-        align="center"
-        _hover={{ opacity: 0.8, cursor: "pointer" }}
-      >
-        <DateRange color={color} from={from} to={to} />
-        <ProjectSummary title={title} summary={summary} keywords={keywords} />
-      </Flex>
-    );
-  },
-);
+export const CareerItem = ({
+  title,
+  summary,
+  keywords,
+  color,
+  from,
+  to,
+}: Props): JSX.Element => {
+  return (
+    <Flex
+      px={4}
+      py={2}
+      w={500}
+      borderWidth={1}
+      borderColor={color}
+      rounded="full"
+      gap={2}
+      h={125}
+      align="center"
+      _hover={{ opacity: 0.8, cursor: "pointer" }}
+    >
+      <DateRange color={color} from={from} to={to} />
+      <ProjectSummary title={title} summary={summary} keywords={keywords} />
+    </Flex>
+  );
+};
--- src/features/about/components/organisms/CareerTimeline.tsx
+++ src/features/about/components/organisms/CareerTimeline.tsx
@@ -1,66 +1,62 @@
-import { type JSX, memo } from "react";
+import { type JSX } from "react";
 import { Circle, Flex, List, ListItem, Box } from "@chakra-ui/react";
 import { BiSolidUpArrow } from "react-icons/bi";
 import { type CareerItems } from "../../types/Career.ts";
 import { CareerItem } from "../molecules/CareerItem.tsx";
 
-export const CareerTimeline = memo(({ items }: CareerItems): JSX.Element => {
+export const CareerTimeline = ({ items }: CareerItems): JSX.Element => {
+  const lineStyle = {
+    position: "absolute",
+    h: "120",
+    w: "0.5",
+    bg: "#4A5668",
+  };
+
   return (
-    <>
-      {/* Convert Timeline based on Flex to based on List components */}
-      <List spacing={6} py={20}>
-        {items.map((item, index) => (
-          <ListItem key={index}>
-            <Flex alignItems="center">
-              <Circle
-                size={8}
-                bg="gray.600"
-                color="white"
-                position="relative"
-                _before={{
-                  content: index === 0 ? '""' : "none",
-                  position: "absolute",
-                  // top: "100%",
-                  bottom: "100%",
-                  w: "0.5",
-                  h: "120",
-                  bg: "#4A5668",
-                  zIndex: -1,
-                }}
-                _after={{
-                  content: '""',
-                  position: "absolute",
-                  top: "100%",
-                  w: "0.5",
-                  h: "120",
-                  bg: "#4A5668",
-                }}
-              >
-                {index === 0 && (
-                  <BiSolidUpArrow
-                    size={20}
-                    style={{
-                      position: "absolute",
-                      bottom: "140px",
-                      color: "#4A5668",
-                    }}
-                  />
-                )}
-                {index + 1}
-              </Circle>
-              <Box w={8} bg="gray.600" h={0.5} />
-              <CareerItem
-                title={item.title}
-                summary={item.summary}
-                keywords={item.keywords}
-                color={item.color}
-                from={item.from}
-                to={item.to}
-              />
-            </Flex>
-          </ListItem>
-        ))}
-      </List>
-    </>
+    <List spacing={6} py={20}>
+      {items.map((item, index) => (
+        <ListItem key={index}>
+          <Flex alignItems="center">
+            <Circle
+              size={8}
+              bg="gray.600"
+              color="white"
+              position="relative"
+              _before={{
+                ...lineStyle,
+                content: index === 0 ? '""' : "none",
+                bottom: "100%",
+              }}
+              _after={{
+                ...lineStyle,
+                content: '""',
+                top: "100%",
+              }}
+            >
+              {index === 0 && (
+                <BiSolidUpArrow
+                  size={20}
+                  style={{
+                    position: "absolute",
+                    bottom: "140px",
+                    color: "#4A5668",
+                  }}
+                />
+              )}
+              {index + 1}
+            </Circle>
+            <Box w={8} bg="gray.600" h={0.5} />
+            <CareerItem
+              title={item.title}
+              summary={item.summary}
+              keywords={item.keywords}
+              color={item.color}
+              from={item.from}
+              to={item.to}
+            />
+          </Flex>
+        </ListItem>
+      ))}
+    </List>
   );
-});
+};

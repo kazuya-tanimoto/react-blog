b208dee847a2f1e99df4b3bfbe8723313cbae1f2
Kazuya Tanimoto <tanimoto@byte-lark.com>
2024-04-26 09:57:07 +0900

    Wrapped by BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -8,8 +9,10 @@ const root = document.getElementById("root");
 root != null &&
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
     </React.StrictMode>,
   );
